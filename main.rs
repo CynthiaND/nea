e9625df8f450712cb57cb6aba622d52e49c0029f
CynthiaND <cynthia.nd@proton.me>
2023-11-30 09:39:39 +0000

    modified postgres.rs to add information functions to interact with the webapi

--- main.rs
+++ main.rs
@@ -1,20 +1,45 @@
-use rocket::fs::NamedFile;
+use std::convert::Infallible;
+use std::net::SocketAddr;
 
-use rocket::response::status::NotFound;
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
 
-pub mod wallet_rpc;
+mod postgres;
 
-#[macro_use]
-extern crate rocket;
-
-#[get("/")]
-async fn index() -> Result<NamedFile, NotFound<String>> {
-    NamedFile::open("html/antibot/index.html")
-        .await
-        .map_err(|e| NotFound(e.to_string()))
+async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // We create a TcpListener and bind it to 127.0.0.1:3000
+    let listener = TcpListener::bind(addr).await?;
+
+    // We start a loop to continuously accept incoming connections
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        // Use an adapter to access something implementing `tokio::io` traits as if they implement
+        // `hyper::rt` IO traits.
+        let io = TokioIo::new(stream);
+
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            // Finally, we bind the incoming connection to our `hello` service
+            if let Err(err) = http1::Builder::new()
+                // `service_fn` converts our function in a `Service`
+                .serve_connection(io, service_fn(hello))
+                .await
+            {
+                println!("Error serving connection: {:?}", err);
+            }
+        });
+    }
 }
--- postgres.rs
+++ postgres.rs
@@ -1,95 +1,160 @@
 use postgres::{Client, NoTls};
-use std::Error;
+use serde::{Deserialize, Serialize};
+use argon2::{
+    password_hash::{
+        rand_core::OsRng,
+        PasswordHasher, SaltString
+    },
+    Argon2
+};
+use uuid::Uuid;
 
-fn client() {
-    Client::connect(
-        "host=localhost user=snow pass=rustontop dbname=flake",
-        NoTls,
-    )?;
+#[derive(Serialize, Deserialize)]
+struct Product {
+    product_id: Uuid,
+    product_name: String,
+    product_image: String,
+    product_description: String,
+    price: u16,
+}
+#[derive(Serialize, Deserialize)]
+struct User {
+    user_id: Uuid,
+    password_hash: String,
+    salt: String,
+    order_ids: Vec<Uuid>,
+}
+#[derive(Serialize, Deserialize)]
+struct Order {
+    order_id: Uuid,
+    cart: Vec<String>,
+    user_id: Uuid,
 }
 
-pub fn init_tables() -> Result<(), Error> {
-    let client = client();
-    let mut transaction = client.transaction()?;
+fn client() -> Client {
+    let client = Client::connect(
+        "host=localhost user=snow pass=rustontop dbname=flake",
+        NoTls,
+    ).unwrap();
+
+    return client
+}
+
+pub fn init_tables() {
+    let mut client = client();
+    let transaction = client.transaction();
+    let mut transaction = transaction.unwrap();
     transaction.batch_execute(
         "
     	CREATE TABLE users (
-    		user_id UUID PRIMARY KEY NOT NULL,
-    		username CHAR(20) NOT NULL,
-    		password CHAR(16) NOT NULL,
+    		userId UUID PRIMARY KEY NOT NULL,
+            email VARCHAR(255) NOT NULL,
+    		passwordHash CHAR(16) NOT NULL,
     		salt CHAR(16) NOT NULL,
-    		pin CHAR(10) NOT NULL,
-    		address CHAR(106) NOT NULL,
             order_ids UUID[],
             );
-        CREATE TABLE user_settings (
-            user_id UUID PRIMARY KEY REFERENCES users,
-            lang CHAR(2) DEFAULT 'EN',
-            fiat CHAR(3) DEFAULT 'USD',
-            dark_mode BOOLEAN DEFAULT TRUE NOT NULL,
-            two_factor_authentication DEFAULT FALSE,
-            pgp_key VARCHAR(4096),
-            );
-        CREATE TABLE vendors (
-            vendor_id UUID PRIMARY NOT NULL,
-            username CHAR(20) NOT NULL,
-            password CHAR(16) NOT NULL,
-            salt CHAR(16) NOT NULL,
-            pin CHAR(10) NOT NULL,
-            rating SMALLINT NOT NULL,
-            address CHAR(106) NOT NULL,
-            listing_ids UUID[] NOT NULL,
-            order_ids UUID[],
-            dispute_ids UUID[],
-            );
-        CREATE TABLE vendor_settings (
-            vendor_id UUID PRIMARY KEY REFERENCES vendors,
-            lang CHAR(2) DEFAULT 'EN',
-            fiat CHAR(3) DEFAULT 'USD',
-            dark_mode BOOLEAN DEFAULT TRUE,
-            pgp_key CHAR(4096) NOT NULL,
-            away BOOLEAN DEFAULT FALSE,
-            );
-        CREATE TABLE orders (
+        CREATE TABLE order (
             order_id UUID PRIMARY KEY NOT NULL,
             items TEXT[] NOT NULL,
-            payment_state CHAR(1) NOT NULL,
-            vendor_id UUID REFERENCES vendors NOT NULL,
             user_id UUID REFERENCES users NOT NULL,
-            af DATE NOT NULL,
             );
-        CREATE TABLE listings (
-            listing_id UUID PRIMARY KEY NOT NULL,
-            name CHAR(50) NOT NULL,
-            listing_type CHAR(10) NOT NULL,
+        CREATE TABLE product (
+            productId UUID PRIMARY KEY NOT NULL,
+            productName CHAR(50) NOT NULL,
+            productImage CHAR(50) NOT NULL,
+            productDescription CHAR(50) NOT NULL,
             price SMALLINT NOT NULL,
-            amounts SMALLINT[],
-            limit SMALLINT,
-            vendor_id UUID REFERENCES vendors,
-            );
-        CREATE TABLE disputes (
-            dispute_id UUID PRIMARY KEY,
-            order_id UUID REFERENCES orders,
-            vendor_id REFERENCES vendors,
-            state CHAR(1) NOT NULL,
-            payment_state CHAR(1) NOT NULL,
-            af DATE NOT NULL,
-            );
-        CREATE TABLE forums (
-            forum_id UUID PRIMARY KEY NOT NULL,
-            title CHAR(20) NOT NULL,
-            description TEXT
-            );
-        CREATE TABLE posts (
-            post_id UUID PRIMARY KEY NOT NULL,
-            title CHAR(50) NOT NULL,
-            body TEXT NOT NULL,
-            rating SMALLINT DEFAULT 0,
-            media TEXT[],
-            forum_id UUID REFERENCES forums,
-            private BOOLEAN DEFAULT FALSE,
             );
         ",
     );
-    transaction.commit()?;
+    transaction.commit();
+}
+
+//Password Hashing Functions
+fn hash_password_existing(password: String, password_salt: String) -> String {
+    let hash_string = SaltString::from_b64(&password_salt).unwrap();
+    let result = Argon2::default().hash_password(password.as_bytes(), &hash_string).unwrap();
+    return result.to_string()
+}
+
+fn hash_password(password: &str) -> (String, String) {
+    let salt = SaltString::generate(&mut OsRng);
+    let argon2 = Argon2::default();
+    let password_hash = argon2.hash_password(password.as_bytes(), &salt).unwrap();
+    return (password_hash.to_string(), salt.to_owned().to_string());
+}
+
+
+//JSON functions
+fn products() -> Vec<String> {
+    let mut client = client();
+    let mut products: Vec<Product> = Vec::new();
+    let mut json_products: Vec<String> = Vec::new();
+    for row in client.query("SELECT productID, productName,productImage, productDescription, price FROM products", &[]).unwrap() {
+        let uuid: String = row.get(0);
+        let price: String = row.get(4);
+        let new_product = Product {
+            product_id: Uuid::parse_str(&uuid).unwrap(),
+            product_name: row.get(1),
+            product_image: row.get(2),
+            product_description: row.get(3),
+            price: str::parse(&price).unwrap(),
+        };
+        products.push(new_product);
+    }
+    for item in products {
+        let json_item = serde_json::to_string(&item);
+        json_products.push(json_item.unwrap());
+    }
+    return json_products
+}
+
+fn product_price(product_id: Uuid) -> String {
+    let mut client = client();
+    let mut price: String = "".into();
+    for row in client.query("SELECT price FROM products WHERE productId = ${product_id}", &[]).unwrap() {
+        price = row.get(0);
+    }
+    return serde_json::to_string(&price).unwrap();
+}
+
+fn product_name(product_id: Uuid) -> String {
+    let mut client = client();
+    let mut name: String = "".into();
+    for row in client.query("SELECT productName FROM products WHERE productId = ${product_id}", &[]).unwrap() {
+        name = row.get(0);
+    }
+    return serde_json::to_string(&name).unwrap();
+}
+
+//User Functions
+fn user_exists(email: String, password: String) -> bool {
+    let mut client = client();
+    let mut password_salt: String;
+    let mut password_hash: String;
+    for user in client.query("SELECT salt, passwordHash FROM users WHERE email = ${email}", &[]).unwrap() {
+        if user.is_empty() {
+            return false;
+        }
+        password_salt = user.get(0);
+        password_hash = user.get(1);
+        if password_salt.is_empty() && password_hash.is_empty() {
+            return false;
+        }
+    if password_hash == hash_password_existing(password.clone(), password_salt) {
+        return true;
+    }
+    }
+    return false
+}
+
+
+fn new_user(email: String, password: String) {
+    let mut client = client();
+    let response = hash_password(&password);
+    let user_id = Uuid::new_v4();
+    client.execute(
+        "INSERT INTO users (userID, email, passwordHash, salt, order_ids) VALUES ($1, $2, $3, $4, $5)",
+        &[&user_id.to_string(), &email, &response.0, &response.1, &""],
+    ).unwrap();
 }
